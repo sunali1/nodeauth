98569620848a3bfd4bef0ecd0cc025058195a1d2
sunaliseth <sunaliseth01@gmail.com>
2017-12-04 17:25:52 +0000

    Test passed it opens on the home page

---
+++ test/index-test.js
@@ -0,0 +1,16 @@
+process.env.NODE_ENV = "test";
+const app = require("../app.js");
+const assert = require("assert");
+const Browser = require("zombie");
+browser = new Browser();
+url = "http://localhost:3000/";
+
+describe("/", function() {
+  it("shows a index page", function(done) {
+    browser.visit(url, function() {
+      assert.ok(this.browser.success);
+      assert.equal(this.browser.text("h2"), "Members Area");
+      done();
+    });
+  });
+});
