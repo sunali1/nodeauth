eca6841381ca497a9ffdaa45d4d1e4433b879e24
sunaliseth <sunaliseth01@gmail.com>
2017-12-18 11:33:12 +0000

    Routes for index, users views

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,7 +9,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register');
+  res.render('register', { title: 'Register' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+  console.log(req.body.name);
+  console.log('1')
 });
 
 module.exports = router;
